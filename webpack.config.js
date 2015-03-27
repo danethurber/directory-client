b5eaf502af467784c4d36beb1b385b70b517ac02
Dane Thurber <dane.thurber@gmail.com>
2015-03-27 14:13:56 -0600

    compile css files

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,14 @@ var config = require('config');
 var webpack = require('webpack');
 
 var ManifestPlugin = require('webpack-manifest-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: path.join(config.assets.entryDir, 'javascripts', 'app')
+    app: [
+      path.join(config.assets.entryDir, 'javascripts', 'app'),
+      path.join(config.assets.entryDir, 'stylesheets', 'app')
+    ]
   },
 
   output: {
@@ -22,15 +26,27 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass?outputStyle=expanded')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css')
       }
     ]
   },
 
   plugins: [
+    new ExtractTextPlugin('[name].[hash].css'),
     new ManifestPlugin()
   ],
 
   resolve: {
-    extensions: ['', '.js']
+    root: [
+      path.join(__dirname, 'node_modules')
+    ],
+    extensions: ['', '.js', '.scss', '.css']
   }
 };
