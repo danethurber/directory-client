9614a3e50cbac6b13c825a7f8ba9835b7a84fe59
Dane Thurber <dane.thurber@gmail.com>
2015-03-31 13:35:26 -0600

    exit if assets fail to build

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,6 +7,7 @@ if (['development', 'production'].indexOf(env) > -1) {
   console.info('Compiling Assets for', env, '\n');
 
   exec('./node_modules/webpack/bin/webpack.js --colors --progress', function(err, stdout){
+    if(err) process.exit(1);
     console.info(stdout);
   });
 }
