1105b208bfccdfef37f8b9984ba4b7af87822d28
Dane Thurber <dane.thurber@gmail.com>
2015-04-02 10:24:31 -0600

    run postinstall in all envs

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,11 +3,9 @@ require('dotenv').load();
 var exec = require('child_process').exec;
 var env = process.env.NODE_ENV;
 
-if (['development', 'production'].indexOf(env) > -1) {
-  console.info('Compiling Assets for', env, '\n');
+console.info('Compiling Assets for', env, '\n');
 
-  exec('./node_modules/webpack/bin/webpack.js --colors --progress', function(err, stdout){
-    if(err) process.exit(1);
-    console.info(stdout);
-  });
-}
+exec('./node_modules/webpack/bin/webpack.js --colors --progress', function(err, stdout){
+  if(err) process.exit(1);
+  console.info(stdout);
+});
